6b28ec44e20fe1f7705f8f4faac8fa7cc2d23f26
MUJAWAMARIYA <mumarg2015@gmail.com>
2019-11-19 16:59:48 +0200

    my first submission on clone downloaded

--- app/models/agenda.rb
+++ app/models/agenda.rb
@@ -2,4 +2,5 @@ class Agenda < ApplicationRecord
   belongs_to :team
   belongs_to :user
   has_many :articles, dependent: :destroy
+  accepts_nested_attributes_for :articles, :allow_destroy => true
 end
